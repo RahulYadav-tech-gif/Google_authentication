aa09e8e655735c00dd3db4051c6ec7238e3f6e60
Rahul Yadav <iamrahulgurjar99@gmail.com>
2024-09-12 16:16:07 +0530

    update changes

--- google/urls.py
+++ google/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', views.login, name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('social-auth/', include('social_django.urls', namespace='social')),
+    path('social-auth/', include('social_django.urls', name='social')),
     path("", views.home, name='home'),
 ]
